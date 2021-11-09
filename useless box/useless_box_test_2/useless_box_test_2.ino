0eb4c8c2d135a64246be8ae334fd3ae9f5084063
Sieger Van Hoof <sieger.vanhoof@student.kdg.be>
2021-11-10 00:00:26 +0100

    test update

--- useless box/useless_box_test_2/useless_box_test_2.ino
+++ useless box/useless_box_test_2/useless_box_test_2.ino
@@ -4,6 +4,7 @@ Servo handServo;
 Servo boxServo;
 
 int switchStatus = 0;
+const int ledPin = 13;
 const int frontSwitchPin = 6;
 const int handServoPin = 10;
 const int boxServoPin = 11;
@@ -11,15 +12,20 @@ const int boxServoPin = 11;
 
 void setup()
 {
-  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
   
+  pinMode(frontSwitchPin, INPUT);
+  digitalWrite(frontSwitchPin, HIGH);
+  
+  Serial.begin(9600);
+
   handServo.attach(handServoPin);
   boxServo.attach(boxServoPin);
-  handServo.write(26);
+  handServo.write(180);
   boxServo.write(70);
 }
 void loop()
-{
+{ 
   switchStatus = digitalRead(frontSwitchPin);
   
 
@@ -27,29 +33,37 @@ void loop()
       for (int i = 70; i <= 110; i++)
       {
         boxServo.write(i);
-        Serial.println("box");
+        Serial.println("box1");
         delay(20);
       }
-      for (int i = 26; i >= 171; i++)
+      delay(20);
+      for (int j = 180; j >= 35; j--)
       {
-        handServo.write(i);
-        Serial.println("hand");
+        handServo.write(j);
+        Serial.println("hand1");
         delay(20);
       }
+      delay(20);
+      
       delay(1000);
-      handServo.write(180);
+      handServo.write(26);
       delay(200);
-      for (int i = 180; i <= 26; i--)
+      
+      for (int y = 26; y <= 180; y++)
       {
-        handServo.write(i);
-        Serial.println("hand");
+        handServo.write(y);
+        Serial.println("hand2");
         delay(20);
       }
-      for (int i = 110; i >= 70; i--)
+      delay(20);
+      
+      for (int x = 110; x >= 70; x--)
       {
-        boxServo.write(i);
-        Serial.println("box");
+        boxServo.write(x);
+        Serial.println("box2");
         delay(20);
       }
+      delay(2000);
+      
   }
 }
